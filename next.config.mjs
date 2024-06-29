aa081ae494b62d7552d3231105298dc439ebc828
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2024-06-29 00:31:27 -0300

    Update gitignore and next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	output: "export",
+	distDir: "dist",
+	images: {
+		unoptimized: true,
+	},
+	reactStrictMode: true,
 };
 
 export default nextConfig;
